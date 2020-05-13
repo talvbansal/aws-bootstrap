157017f16e3253deb4f6e656b6981c35f72f2b3c
Talv Bansal <talvbansal@outlook.com>
2020-05-13 22:33:35 +0100

    WIP

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-const { hostname } = require('os');
-const http = require('http');
+const { hostname } = require("os");
+const http = require("http");
 
-const message = 'Hello World\n';
+const message = "Hello Cloud\n";
 const port = 8080;
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end(message);
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end(message);
 });
-
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname()}:${port}/`);
--- stop-service.sh
+++ stop-service.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -xe
-source /home/es2-user/.bash_profile
+source /home/ec2-user/.bash_profile
 [ -d "/home/ec2-user/app/release" ] && \
 cd /home/ec2-user/app/release && \
 npm stop

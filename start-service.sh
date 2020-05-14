ceb70ccebd3fe89598dd2196e2606cd041f8c08a
Talv Bansal <talvbansal@outlook.com>
2020-05-14 19:52:27 +0100

    WIP

--- start-service.sh
+++ start-service.sh
@@ -3,8 +3,7 @@ source /home/ec2-user/.bash_profile
 cd /home/ec2-user/app/release
 
 # Query The EC2 metadata service for this instances region...
-REGION=`curl -s http://169.254.169.254/latest/dynamic/instance-
-identity/document | jq .region -r`
+REGION=`curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq .region -r`
 
 # Query the EC2 metadata service for the instances instance-id...
 export INSTANCE_ID=`curl -s http://169.254.169.254/latest/meta-data/instance-id`
